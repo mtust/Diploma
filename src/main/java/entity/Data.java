a24735770cd841deabcd832446353f283751b197
mtust <mtustanovskyy@gmail.com>
2016-08-27 18:24:46 +0300

    first commit

---
+++ src/main/java/entity/Data.java
@@ -0,0 +1,15 @@
+package entity;
+
+import java.util.List;
+
+/**
+ * Created by tust on 27.08.2016.
+ */
+public class Data {
+
+    private List<Double> trainVector;
+    private List<Double> useVector;
+
+
+
+}
